ab26826e4e65163a540567df74175305d3c7f547
Navas shareef <navasshareef188@gmail.com>
2021-09-12 17:20:23 +0530

    heroku deployment 1

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@pn$h@3p&v5sj*x9=$&%m&i#9%0#jp)v45#%+!oqbl=v%d-%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['puzzle99.herokuapp.com','127.0.0.1']
 
 
 # Application definition
