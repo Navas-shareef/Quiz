5332d17edf0788504e1a492bec562bbc38fe0054
Navas shareef <navasshareef188@gmail.com>
2021-09-12 17:55:33 +0530

    heroku deploy 5

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['puzzle99.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'app',
     'crispy_forms',
     'django.contrib.admin',
@@ -124,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
