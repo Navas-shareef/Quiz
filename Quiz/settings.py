7fd93f7a5a18a279aee454b93fd3fa2752eb3fe2
Navas shareef <navasshareef188@gmail.com>
2021-09-12 18:54:10 +0530

    heroku deploy 13

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -124,10 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT =  os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'media/images')
+    os.path.join(BASE_DIR,'static')
 
 ]
 
@@ -138,6 +139,9 @@ STATIC_URL = '/static/'
 #media url to save images anf files
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 
 
