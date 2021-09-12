c71c999c3c8347abdc786fe5cb07884b920d3953
Navas shareef <navasshareef188@gmail.com>
2021-09-12 19:44:52 +0530

    heroku deploy 23

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -127,10 +127,7 @@ USE_TZ = True
 
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'staticfiles')
 
-]
 
 
 STATIC_URL = '/static/'
@@ -138,7 +135,7 @@ STATIC_URL = '/static/'
 
 #media url to save images anf files
 MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
 
 
 
